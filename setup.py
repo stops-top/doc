962115e606076f37d5bf52b8639228fe1baf7230
qitas <qitas@qitas.cn>
2021-08-04 13:39:59 +0800

    fix

---
+++ setup.py
@@ -0,0 +1,43 @@
+
+
+from setuptools import find_packages, setup
+
+
+setup(
+    package_data={
+        "platformio": [
+            "ide/tpls/*/.*.tpl",
+            "ide/tpls/*/*.tpl",
+            "ide/tpls/*/*/*.tpl",
+            "ide/tpls/*/.*/*.tpl",
+        ],
+        "scripts": ["99-platformio-udev.rules"],
+    },
+    entry_points={
+        "console_scripts": [
+            "platformio = platformio.__main__:main",
+            "pio = platformio.__main__:main",
+            "piodebuggdb = platformio.__main__:debug_gdb_main",
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: C",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Build Tools",
+        "Topic :: Software Development :: Compilers",
+    ],
+    keywords=[
+        "iot",
+        "embedded",
+        "hardware",
+        "microcontroller",
+        "debug",
+    ],
+)
