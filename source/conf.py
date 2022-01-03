8dca6cb70679f2f0241048829bb37ef371c54b9d
qitas <qitas@qitas.cn>
2022-01-03 17:35:11 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,7 @@ master_doc = "index"
 
 # General information about the project.
 project = u"STOPs"
-copyright = u"2021, STOPs.top"
+copyright = u"2022, STOPs.top"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
